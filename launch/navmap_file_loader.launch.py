f956d7327507b70aa3adae58dba73d7ef800b8d3
gglaspell <garry.p.glaspell@usace.army.mil>
2026-03-24 12:09:12 -0400

    system main launch file

--- launch/navmap_file_loader.launch.py
+++ launch/navmap_file_loader.launch.py
@@ -1,5 +1,4 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -12,18 +11,14 @@ def generate_launch_description():
         "navmap_file_loader.yaml",
     )
 
-    maps_manager_node = Node(
-        package="easynav_system",        # <-- changed from easynav_maps_manager
-        executable="maps_manager_node",
-        name="maps_manager_node",
+    system_node = Node(
+        package="easynav_system",
+        executable="system_main",
+        name="easynav_system",
         output="screen",
         parameters=[config_file],
-        # Uncomment if downstream EasyNav nodes expect a bare /navmap topic:
-        # remappings=[
-        #     ("maps_manager_node/navmap/map", "navmap"),
-        # ],
     )
 
     return LaunchDescription([
-        maps_manager_node,
+        system_node,
     ])
